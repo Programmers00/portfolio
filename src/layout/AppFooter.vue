e053a79ffb99c6197497821e3f1c0c38f053c427
eunshinNoh <nes0410@gmail.com>
2022-10-04 12:38:06 -0600

    refs #17 footer image, font size

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,15 +1,17 @@
 <template>
   <footer class="mainFooterBox">
     <ul class="footerIconsBox">
+      <!-- footer icon: repeat-->
       <div v-for="{ url, imgSrc } in iconsData" v-bind:key="url">
         <a :href="url" target="_blank">
           <img
             :src="`/assets/images/${imgSrc}.png`"
             :alt="`${imgSrc}`"
-            height="65"
+            height="50rem"
         /></a>
       </div>
     </ul>
+    <!-- copyright -->
     <a class="footerCopyright" href="#"
       >Copyright © NOhBUG all rights reserved.</a
     >
@@ -21,6 +23,7 @@ export default {
   name: "AppFooter",
   components: {},
   data: () => ({
+    /** data */
     iconsData: [
       {
         url: "https://www.linkedin.com/in/eunshin-noh-b58a06223/",
@@ -71,8 +74,14 @@ export default {
   }
   .footerCopyright {
     font-weight: 400;
-    font-size: 20px;
-    line-height: 24px;
+    // font-size: 20px;
+    // line-height: 24px;
+    font-size: 1rem;
+    line-height: 1rem;
+    @include lg {
+      font-size: 1.3rem;
+      line-height: 1.3rem;
+    }
   }
 }
 </style>
